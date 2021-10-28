c28f9af96c0b4b12019966d4e96fe924b496a8d6
sindm <sinitsyn.d.i@gmail.com>
2021-10-28 19:56:20 +0300

    Fix controller

--- src/main/java/ru/sindm/practikum/logistic/app/domain/controller/DeliveryController.java
+++ src/main/java/ru/sindm/practikum/logistic/app/domain/controller/DeliveryController.java
@@ -1,9 +1,9 @@
 package ru.sindm.practikum.logistic.app.domain.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.sindm.practikum.logistic.app.domain.common.model.Response;
 import ru.sindm.practikum.logistic.app.domain.exception.DeliveryPriceCalculateException;
+import ru.sindm.practikum.logistic.app.domain.model.DeliveryPrice;
 import ru.sindm.practikum.logistic.app.domain.model.DeliveryPriceRequest;
 import ru.sindm.practikum.logistic.app.domain.service.DeliveryPriceService;
 
@@ -20,7 +20,7 @@ public class DeliveryController {
     }
 
     @PostMapping("/price/calculate")
-    public ResponseEntity<?> calculateDeliveryPrice(@RequestBody @Valid DeliveryPriceRequest request) throws DeliveryPriceCalculateException {
-        return new ResponseEntity<>(deliveryPriceService.calculateDeliveryPrice(request), HttpStatus.OK);
+    public Response<DeliveryPrice> calculateDeliveryPrice(@RequestBody @Valid DeliveryPriceRequest request) throws DeliveryPriceCalculateException {
+        return new Response<>(200, deliveryPriceService.calculateDeliveryPrice(request));
     }
 }
