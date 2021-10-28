533efa3d3ba0477306350692658a05373e4810ff
sindm <sinitsyn.d.i@gmail.com>
2021-10-28 19:57:04 +0300

    Fix advice

--- src/main/java/ru/sindm/practikum/logistic/app/domain/common/utils/AppErrorAdvice.java
+++ src/main/java/ru/sindm/practikum/logistic/app/domain/common/utils/AppErrorAdvice.java
@@ -1,37 +1,30 @@
 package ru.sindm.practikum.logistic.app.domain.common.utils;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.sindm.practikum.logistic.app.domain.common.model.Response;
 import ru.sindm.practikum.logistic.app.domain.exception.DeliveryPriceCalculateException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class AppErrorAdvice {
 
     @ExceptionHandler(DeliveryPriceCalculateException.class)
-    public ResponseEntity<?> handleDeliveryPriceCalculateException(DeliveryPriceCalculateException e) {
-        return buildResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public Response<?> handleDeliveryPriceCalculateException(DeliveryPriceCalculateException e) {
+        return buildResponseEntity(500, e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return buildResponseEntity(
+    public Response<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return buildResponseEntity(400,
                 e.getBindingResult()
                         .getFieldErrors()
                         .stream().findFirst()
-                        .map(DefaultMessageSourceResolvable::getDefaultMessage), HttpStatus.BAD_REQUEST);
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage).get());
     }
 
-    private ResponseEntity<?> buildResponseEntity(Object error, HttpStatus status) {
-        Map<String, Object> body = new HashMap<>() {{
-            put("error", error);
-        }};
-        return new ResponseEntity<>(body, status);
+    private Response<?> buildResponseEntity(Integer code, String error) {
+        return new Response<>(code, error);
     }
 }
